16c409559f70d50afaf82fc56740f28660772316
Favour Oghenekowho <gh.oghene@gmail.com>
2023-02-25 01:54:24 +0100

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const title = 'Blog Post';
+  const body = 'My blog post about technology!';
+  const comments = [
+    { id: 1, text: 'Comment one' },
+    { id: 2, text: 'Comment two' },
+    { id: 3, text: 'Comment three' },
+    { id: 4, text: 'Comment four' },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>{title}</h1>
+      <p>{body}</p>
+      <div className="comments">
+        <h2>comments({comments.length})</h2>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
