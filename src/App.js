46e600c885b3eeed36743f575cfca4a6feaf8e04
Favour Oghenekowho <gh.oghene@gmail.com>
2023-03-01 00:01:28 +0100

    created feedback list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
+import { useState } from 'react';
+
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import feedbackData from './data/feedbackData';
+
 function App() {
-  const title = 'Blog Post';
-  const body = 'My blog post about technology!';
-  const comments = [
-    { id: 1, text: 'Comment one' },
-    { id: 2, text: 'Comment two' },
-    { id: 3, text: 'Comment three' },
-    { id: 4, text: 'Comment four' },
-  ];
+  const [feedback, setFeedback] = useState(feedbackData);
 
   return (
-    <div className="container">
-      <h1>{title}</h1>
-      <p>{body}</p>
-      <div className="comments">
-        <h2>comments({comments.length})</h2>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackList feedback={feedback} />
       </div>
-    </div>
+    </>
   );
 }
 
